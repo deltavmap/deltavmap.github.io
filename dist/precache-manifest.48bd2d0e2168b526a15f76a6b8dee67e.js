38845cf0680226ddaa06b1eeef047eee70b527e3
James Devon <jamesdevon@protonmail.com>
2021-06-02 19:39:17 +0100

    build for production

---
+++ dist/precache-manifest.48bd2d0e2168b526a15f76a6b8dee67e.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "54e07ea42ce4d944bee8",
+    "url": "/deltavmap/css/app.3e8eaffd.css"
+  },
+  {
+    "revision": "57ba5e4c61897a180c49",
+    "url": "/deltavmap/css/chunk-vendors.6b4fd6b3.css"
+  },
+  {
+    "revision": "53f89a581e8abc61a10f728f4dd74123",
+    "url": "/deltavmap/index.html"
+  },
+  {
+    "revision": "54e07ea42ce4d944bee8",
+    "url": "/deltavmap/js/app.36a20065.js"
+  },
+  {
+    "revision": "57ba5e4c61897a180c49",
+    "url": "/deltavmap/js/chunk-vendors.802080c3.js"
+  },
+  {
+    "revision": "61ed7445b726b2381b401adad64044ef",
+    "url": "/deltavmap/manifest.json"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/deltavmap/robots.txt"
+  }
+]);
