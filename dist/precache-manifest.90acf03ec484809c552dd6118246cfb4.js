5d23193ec19247b99a712515ca44abf985d8acad
deltavmap <deltavmap@gmail.com>
2021-06-05 10:45:54 +0100

    build for production

---
+++ dist/precache-manifest.90acf03ec484809c552dd6118246cfb4.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d9e17f7a23dee3b60fd4",
+    "url": "/css/app.907dc9ab.css"
+  },
+  {
+    "revision": "3b0bbf0bb77fcd9780ba",
+    "url": "/css/chunk-vendors.bae8be4d.css"
+  },
+  {
+    "revision": "245ee82ab989766593b9c1d7d0f8cf31",
+    "url": "/index.html"
+  },
+  {
+    "revision": "d9e17f7a23dee3b60fd4",
+    "url": "/js/app.e0267002.js"
+  },
+  {
+    "revision": "3b0bbf0bb77fcd9780ba",
+    "url": "/js/chunk-vendors.66390ab8.js"
+  },
+  {
+    "revision": "61ed7445b726b2381b401adad64044ef",
+    "url": "/manifest.json"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/robots.txt"
+  }
+]);
