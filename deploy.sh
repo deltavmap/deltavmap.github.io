964911f8130fc8064c40e2969cfe02c61745e86c
deltavmap <deltavmap@gmail.com>
2021-06-17 14:44:00 +0100

    ensure deploy script starts with fresh branch

--- deploy.sh
+++ deploy.sh
@@ -10,40 +10,79 @@
 # set -e # TODO re-enable
 
 function gitstash() {
-    git stash push -m "zsh_stash_name_$1"
+  stash_name="zsh_stash_name_$1"
+  git stash push -m "$stash_name"
 }
 
 function gitstashapply() {
-    git stash apply $(git stash list | grep "zsh_stash_name_$1" | cut -d: -f1)
+  stash_name=$(git stash list | grep "zsh_stash_name_$1" | cut -d: -f1)
+  if [ -n "$stash_name" ]
+  then
+    echo "applying stash $stash_name"
+    git stash apply "$stash_name"
+  else
+    echo "no stash found"
+  fi
 }
 
-date=$(date +%s)
-branch='gh-temp-branch'
-remote_branch='gh-branch2'
+date_stamp=$(date +%s)
+temp_local_branch='gh-temp-branch'
+remote_branch='gh-pages2'
 current_branch=$(git branch --show-current)
 
 
 echo "==================="
 echo "==== DEPLOYING ===="
-echo "deploying to $branch"
+echo
+echo "deploying to >>> $remote_branch <<<"
 echo ""
 
-echo "GIT STASH"; # TODO gitstash $date
+echo "GIT STASH"; gitstash $date_stamp
 
-echo "GIT CHECKOUT $branch"; git checkout $branch
+echo "GIT CHECKOUT MAIN"; git checkout main;
 
-echo "GIT MERGE MAIN"; git merge main
+echo "GIT DELETE BRANCH $temp_local_branch"; git branch -D $temp_local_branch ;
 
-echo "BUILD DIST"; npm run build
+echo "GIT CHECKOUT $temp_local_branch"; git checkout -b $temp_local_branch ;
 
-echo "CREATE VERSION.TXT"; echo $date > 'dist/version.txt'
+echo "BUILD DIST"; npm run build ;
 
-echo "ADDING DIST"; git add dist
+echo "CREATE VERSION.TXT"; echo "$date_stamp" > 'dist/version.txt' ;
 
-echo "GIT COMMIT"; git commit -m "build for production" --no-verify &> /dev/null
+#echo "ADDING DIST"; git add dist ;
+#
+#echo "GIT COMMIT"; git commit -m "build for production" --no-verify &> /dev/null ;
+#
+#echo "GIT PUSH SUBTREE";
+#git push origin `git subtree split --prefix dist "$temp_local_branch"`:$remote_branch --force ;
 
-echo "GIT PUSH SUBTREE"; git subtree push --prefix dist origin $remote_branch
+echo "CD DIST";
+cd dist
 
-echo "RETURN TO MAIN BRANCH"; git checkout $current_branch
+echo "GIT INIT";
+git init
+# git config credential.username deltavmap
+# git config user.email deltavmap@gmail.com
 
-echo "GIT STASH APPLY"; gitstashapply $date
+echo "GIT ADD REMOTE";
+git remote add origin git@github.com:deltavmap/deltavmap.github.io.git
+# git push --set-upstream origin:gh-pages2 master
+
+echo "GIT ADD ."
+git add .
+
+echo "GIT COMMIT"
+git commit -am "deploy"
+
+echo "GIT PUSH"
+git push --force origin master:gh-pages2
+
+echo "RM -RF"
+rm -rf .git
+cd ..
+
+echo "RETURN TO MAIN BRANCH"; git checkout "$current_branch" ;
+
+echo "GIT RESET HEAD"; git reset HEAD --hard ;
+
+echo "GIT STASH APPLY"; gitstashapply "$date_stamp" ;
